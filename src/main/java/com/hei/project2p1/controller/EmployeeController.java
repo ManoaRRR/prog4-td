eae831d1fefe5123a66a2f4176e9f8ba963bb9fa
ManoaRRR <hei.manoa.2@gmai.com>
2023-07-30 21:31:52 +0300

    feat: features export csv

--- src/main/java/com/hei/project2p1/controller/EmployeeController.java
+++ src/main/java/com/hei/project2p1/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.hei.project2p1.controller;
 
 import com.hei.project2p1.model.Employee;
 import com.hei.project2p1.service.EmployeeService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -27,8 +29,7 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Employee> employees = employeeService.getEmployees();
-        model.addAttribute("employees", employees);
+        model.addAttribute("employees", employeeService.getEmployees());
         model.addAttribute("newEmployee", new Employee());
         return "index";
     }
@@ -72,30 +73,15 @@ public class EmployeeController {
     @GetMapping("/updateEmployeeForm/{id}")
     public String showUpdateEmployeeForm(@PathVariable Long id, Model model) {
         Employee employee = employeeService.getEmployeeById(id)
-                .orElseThrow(() -> new IllegalArgumentException("ID d'employé invalide : " + id));
-
+                .orElseThrow(() -> new IllegalArgumentException("Invalid employee ID: " + id));
         model.addAttribute("employee", employee);
         return "update_employee";
     }
 
-
-    @GetMapping("/updateEmployee/{id}")
-    public String showUpdateForm(@PathVariable Long id, Model model) {
-        Optional<Employee> employeeOptional = employeeService.getEmployeeById(id);
-
-        if (employeeOptional.isPresent()) {
-            Employee employee = employeeOptional.get();
-            model.addAttribute("employee", employee);
-            return "update_employee";
-        } else {
-            return "redirect:/";
-        }
-    }
-    @PostMapping("/saveUpdateEmployee/{id}")
-    public String saveUpdate(@PathVariable Long id, @ModelAttribute Employee updatedEmployee) {
-        Employee existingEmployee = employeeService.getEmployeeById(id)
-                .orElseThrow(() -> new IllegalArgumentException("ID d'employé invalide : " + id));
-
+    @PostMapping("/saveUpdateEmployee")
+    public String saveUpdateEmployee(@ModelAttribute Employee updatedEmployee) {
+        Employee existingEmployee = employeeService.getEmployeeById(updatedEmployee.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid employee ID: " + updatedEmployee.getId()));
         existingEmployee.updateEmployee(updatedEmployee);
         employeeService.save(existingEmployee);
         return "redirect:/";
@@ -103,8 +89,7 @@ public class EmployeeController {
 
     @GetMapping("/filterEmployees")
     public String filterEmployeesByKeyword(@RequestParam("attribute") String attribute,
-                                           @RequestParam("value") String value,
-                                           Model model) {
+                                           @RequestParam("value") String value, Model model) {
         List<Employee> filteredEmployees = employeeService.filterEmployeesByAttributeAndValue(attribute, value);
         model.addAttribute("employees", filteredEmployees);
         model.addAttribute("newEmployee", new Employee());
@@ -113,8 +98,7 @@ public class EmployeeController {
 
     @GetMapping("/filterEmployeesByAttribute")
     public String filterEmployeesByAttribute(@RequestParam String attribute,
-                                             @RequestParam String value,
-                                             Model model) {
+                                             @RequestParam String value, Model model) {
         List<Employee> filteredEmployees = employeeService.filterEmployeesByAttribute(attribute, value);
         model.addAttribute("employees", filteredEmployees);
         model.addAttribute("newEmployee", new Employee());
@@ -128,6 +112,73 @@ public class EmployeeController {
         model.addAttribute("newEmployee", new Employee());
         return "index";
     }
+
+    @GetMapping("/exportCSV")
+    public void exportCSV(@RequestParam(value = "attribute", required = false) String attribute,
+                          @RequestParam(value = "value", required = false) String value,
+                          HttpServletResponse response) throws IOException {
+
+        // Obtenez la liste d'employés filtrée si des attributs de filtrage sont fournis
+        List<Employee> employees;
+        if (attribute != null && value != null) {
+            employees = employeeService.filterEmployeesByAttributeAndValue(attribute, value);
+        } else {
+            employees = employeeService.getEmployees();
+        }
+
+        // Générez le contenu CSV à partir de la liste d'employés
+        StringBuilder csvContent = new StringBuilder();
+        csvContent.append("ID,First Name,Last Name,Birth Date,Sex,Phones,Address,Personal Email,Work Email,CIN Number,CIN Date of Issue,CIN Place of Issue,Job Title,Children Count,Hire Date,Departure Date,Socio-Professional Category,CNAPS Number\n");
+        for (Employee employee : employees) {
+            csvContent.append(String.format("%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s\n",
+                    employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getBirthDate(),
+                    employee.getSex(), employee.getPhones(), employee.getAddress(), employee.getPersonalEmail(),
+                    employee.getWorkEmail(), employee.getCinNumber(), employee.getCinDateOfIssue(),
+                    employee.getCinPlaceOfIssue(), employee.getJobTitle(), employee.getChildrenCount(),
+                    employee.getHireDate(), employee.getDepartureDate(), employee.getSocioProfessionalCategory(),
+                    employee.getCnapsNumber()));
+        }
+
+        // Configurez la réponse HTTP pour le téléchargement du fichier CSV
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=\"employee_list.csv\"");
+
+        // Écrivez le contenu CSV dans la réponse
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(csvContent.toString());
+        }
+    }
+
+    @GetMapping("/exportAllEmployeesCSV")
+    public void exportAllEmployeesCSV(HttpServletResponse response) throws IOException {
+        // Récupérer toutes les données des employés depuis la base de données
+        List<Employee> employees = employeeService.getEmployees();
+
+        // Générez le contenu CSV à partir de la liste d'employés
+        StringBuilder csvContent = new StringBuilder();
+        csvContent.append("ID,First Name,Last Name,Birth Date,Sex,Phones,Address,Personal Email,Work Email,CIN Number,CIN Date of Issue,CIN Place of Issue,Job Title,Children Count,Hire Date,Departure Date,Socio-Professional Category,CNAPS Number\n");
+        for (Employee employee : employees) {
+            csvContent.append(String.format("%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%s,%s,%s,%s\n",
+                    employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getBirthDate(),
+                    employee.getSex(), employee.getPhones(), employee.getAddress(), employee.getPersonalEmail(),
+                    employee.getWorkEmail(), employee.getCinNumber(), employee.getCinDateOfIssue(),
+                    employee.getCinPlaceOfIssue(), employee.getJobTitle(), employee.getChildrenCount(),
+                    employee.getHireDate(), employee.getDepartureDate(), employee.getSocioProfessionalCategory(),
+                    employee.getCnapsNumber()));
+        }
+
+        // Configurez la réponse HTTP pour le téléchargement du fichier CSV
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=\"all_employees.csv\"");
+
+        // Écrivez le contenu CSV dans la réponse
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(csvContent.toString());
+        }
+    }
+
+
+
 }
 
 
